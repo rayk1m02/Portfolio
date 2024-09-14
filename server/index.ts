175c2a9725c947617b4d63e2ca76801e3df54d85
rayk1m02 <hkapray@gmaill.com>
2024-09-14 15:28:52 -0700

    Establish connection to MongoDB

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,9 @@
 // express is a web fraemwork for Node.js that simplifies process of building server-side apps and APIs.
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+const port = 5000;
 // JSON parsing middleware
 app.use(express.json());
 
@@ -10,7 +12,14 @@ app.get('/', (req, res) => {
   res.send('Backend is running');
 });
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
-});
+const MONGO_URI = 'your-mongo-db-string';
 
+// establish connection to MongoDB database
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch(err => console.error('MongoDB connection error:', err));
