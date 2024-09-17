9bdb44400369e599e93647fd3b493f05027560c3
rayk1m02 <hkapray@gmaill.com>
2024-09-16 20:26:32 -0700

    in server dir, added src dir with routes dir

--- server/dist/index.js
+++ server/dist/index.js
@@ -7,7 +7,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config({ path: '../.env' });
+// dotenv.config({path: '../.env'});
+dotenv_1.default.config();
 const app = (0, express_1.default)();
 // port 5000 taken by ControlCe (Apple)
 const port = 5001;
