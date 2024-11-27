ecf050f734567dfbaa95a239c4476872dc17426d
rayk1m02 <hkapray@gmaill.com>
2024-11-26 20:16:47 -0600

    experiences tab

--- client/src/pages/Experience.tsx
+++ client/src/pages/Experience.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Experience: React.FC = () => {
+const Experience = () => {
   return (
     <div className="min-h-screen pt-20">
       <h1 className="text-3xl font-bold mb-6">Experience</h1>
       <div className="max-w-3xl mx-auto">
         {/* Add experience content here */}
-        <p>An interactive timeline of my professional experiences</p>
+        <p>My experience will be displayed here.</p>
       </div>
     </div>
   );
