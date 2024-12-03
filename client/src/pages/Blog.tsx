cdda19c9d1d7a4e62a033cabd14fef5ec26c75ba
rayk1m02 <hkapray@gmaill.com>
2024-12-03 17:52:44 -0600

    blog

--- client/src/pages/Blog.tsx
+++ client/src/pages/Blog.tsx
@@ -5,7 +5,7 @@ const Blog: React.FC = () => {
     <div className="min-h-screen bg-gradient-to-b from-secondary-dark to-primary-800 text-secondary-light pt-20">
       <div className="max-w-3xl mx-auto mt-10">
         {/* Add blog content here */}
-        <p>My blog posts will be displayed here. Blogs can be about personal learnings or anything else I find interesting in life.</p>
+        <p>Anything I find interesting in life.</p>
       </div>
     </div>
   );
