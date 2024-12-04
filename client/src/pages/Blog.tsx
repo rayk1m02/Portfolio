e82f298447860434f1f83c6d2e94313d62ef61db
rayk1m02 <hkapray@gmaill.com>
2024-12-04 16:43:03 -0600

    sum

--- client/src/pages/Blog.tsx
+++ client/src/pages/Blog.tsx
@@ -5,7 +5,7 @@ const Blog: React.FC = () => {
     <div className="min-h-screen bg-gradient-to-b from-secondary-dark to-primary-800 text-secondary-light pt-20">
       <div className="max-w-3xl mx-auto mt-10">
         {/* Add blog content here */}
-        <p>Anything I find interesting in life.</p>
+        <p>Anything I find interesting.</p>
       </div>
     </div>
   );
