ef5f932e3848a562059c7d5c6d04ed8badd4cfb3
rayk1m02 <hkapray@gmaill.com>
2024-12-09 18:37:15 -0600

    mock data

--- client/src/api/intro.ts
+++ client/src/api/intro.ts
@@ -3,15 +3,23 @@ import axiosInstance from './axiosInstance';
 
 export const getIntroData = async () => {
 
-  try {
-    const response = await axiosInstance.get('/intro'); 
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching intro data:', error);
-    if (axios.isAxiosError(error)) {
-      console.error('It is Axios error:', error.response?.data);
-    }
-    throw error;
-  }
+  // try {
+  //   const response = await axiosInstance.get('/intro'); 
+  //   return response.data;
+  // } catch (error) {
+  //   console.error('Error fetching intro data:', error);
+  //   if (axios.isAxiosError(error)) {
+  //     console.error('It is Axios error:', error.response?.data);
+  //   }
+  //   throw error;
+  // }
+
+  return {
+    name: "Raymond Kim",
+    email: "hkapray@gmail.com",
+    phone: "312.383.7496",
+    linkedIn: "https://www.linkedin.com/in/raykim02",
+    github: "https://github.com/rayk1m02"
+  };
 
 };
